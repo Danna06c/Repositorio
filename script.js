a23f420d57fdd14e96e22978bc2abafededc844a
Danna06c <dannacardenasbeltran34@gmail.com>
2024-08-05 15:04:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        var loader = document.getElementById('loader');
-        loader.style.opacity = '0';
-        loader.style.transition = 'opacity 0.5s ease';
-        loader.addEventListener('transitionend', function() {
-            loader.style.display = 'none';
-            var content = document.getElementById('content');
-            content.classList.add('show');
-        });
-    }, 1000); 
-});
-
-const container = document.querySelector('.container');
-const btnSingIn = document.getElementById("btn-sign-in");
-const btnSingUp = document.getElementById("btn-sign-up");
-
-btnSingIn.addEventListener('click',()=>{
-   container.classList.remove("toggle");
-});
-
-btnSingUp.addEventListener('click',()=>{
-    container.classList.add("toggle");
-});
+const menuToggle = document.querySelector('.menuToggle');
+    menuToggle.onclick = function () 
+{
+    menuToggle.classList.toggle('active')
+}
